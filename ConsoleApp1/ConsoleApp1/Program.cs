bdda58e7b8f1bcea2cc0e9addab9292630d073e3
Krzysztof Porebski <krzysztof.porebski@gaincapital.com>
2019-09-11 15:02:22 +0200

    init

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using NodaTime;
+using NodaTime.Extensions;
+using NodaTime.TimeZones;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            var timeSpan = TimeSpan.Parse("0030");
+
+            Console.WriteLine(timeSpan);
+
+            
+            // var losAngeles = DateTimeZoneProviders.Tzdb["America/Los_Angeles"];
+            // var vancouver = DateTimeZoneProviders.Tzdb["America/Vancouver"];
+            //
+            // var instant = SystemClock.Instance.GetCurrentInstant();
+            // ZonedDateTime losAngelesDateTime = instant.InZone(losAngeles);
+            // ZonedDateTime vancouverDateTime = instant.InZone(vancouver);
+            //
+            //
+            // TzdbDateTimeZoneSource.Default.WindowsMapping.PrimaryMapping.TryGetValue("Dateline Standard Time", out var buba);
+            //
+            // TimeZoneInfo findSystemTimeZoneById = TimeZoneInfo.FindSystemTimeZoneById("Mid-Atlantic Standard Time");
+            
+            
+
+            // for (int i = 0; i < 365; i++)
+            // {
+            //     if (losAngelesDateTime.IsDaylightSavingTime() != vancouverDateTime.IsDaylightSavingTime())
+            //     {
+            //         Console.WriteLine(losAngelesDateTime);
+            //         Console.WriteLine(vancouverDateTime);
+            //         
+            //         Console.WriteLine("Buuba");
+            //     }
+            //     losAngelesDateTime = losAngelesDateTime.Plus(Duration.FromDays(1));
+            //     vancouverDateTime = vancouverDateTime.Plus(Duration.FromDays(1));
+            // }
+
+            Console.WriteLine("Ok");
+
+            // ReadOnlyCollection<TimeZoneInfo> readOnlyCollection = TimeZoneInfo.GetSystemTimeZones();
+            // foreach (var timeZoneInfo in readOnlyCollection)
+            // {
+            //     Console.WriteLine(timeZoneInfo.StandardName);
+            // }
+            //
+            // Console.WriteLine(readOnlyCollection.Count);
+            //
+            // Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Apache.NMS;
+using Apache.NMS.AMQP;
+using GainCapital.NMS;
+using NLog;
+using Spring.Messaging.Nms.Connections;
+using Spring.Messaging.Nms.Core;
+using Spring.Messaging.Nms.Listener;
+
+namespace FailoverExampleApp
+{
+    static class Program
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        private static readonly string DestinationName = "myTestDestination";
+
+        static void Main(string[] args)
+        {
+            Tracer.Trace = new NLogAdapter();
+
+            var brokerUri = "failover:(amqp://127.0.0.1:5672,amqp://127.0.0.1:5673)" +
+                            "?failover.initialReconnectDelay=1000" +
+                            "&failover.reconnectDelay=1000" +
+                            "&failover.maxReconnectAttempts=10";
+            var connectionFactory = new NmsConnectionFactory(brokerUri);
+            var simpleMessageListenerContainer = new SimpleMessageListenerContainer
+            {
+                ConnectionFactory = connectionFactory,
+                DestinationName = DestinationName,
+                MessageListener = new MessageListener()
+            };
+            // start listener
+            simpleMessageListenerContainer.AfterPropertiesSet();
+            
+            var cachingConnectionFactory = new CachingConnectionFactory(connectionFactory);
+            var nmsTemplate = new NmsTemplate(cachingConnectionFactory)
+            {
+                DefaultDestinationName = DestinationName,
+            };
+
+            while (true)
+            {
+                Logger.Info("Enter your message.");
+                var text = Console.ReadLine();
+                nmsTemplate.SendWithDelegate(session => session.CreateTextMessage(text));
+            }
+        }
+    }
+
+    public class MessageListener : IMessageListener
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        public void OnMessage(IMessage message)
+        {
+            switch (message)
+            {
+                case ITextMessage textMessage:
+                    Logger.Info($"Received message: {textMessage}");
+                    break;
+                default:
+                    Logger.Error("Received unknown message.");
+                    break;
+            }
+        }
+    }
+}
